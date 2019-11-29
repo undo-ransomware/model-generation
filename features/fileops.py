d065220743870ae30dd75b9e0d57e343b8919c9f
Matthias Fratz <matthias.fratz@uni-konstanz.de>
2019-11-29 16:21:59 +0100

    track margin to analysis timeout
    and warn on cross-directory renames. these should work just fine but
    can make tracking phantom files very hard.

--- features/fileops.py
+++ features/fileops.py
@@ -16,6 +16,7 @@ MAX_DURATION = 10 # seconds
 USERDIR = 'C:\\Users\\cuckoo'
 MAX_TIME_DIFF = 10 # seconds
 MAX_DURATION_DIFF = 1 # seconds
+TIMEOUT_MARGIN = timedelta(seconds=300)
 WINDOWS_JUNK = ['AppData', 'NTUSER.DAT']
 
 def isoparse(isotime):
@@ -50,6 +51,9 @@ def is_windows_junk(path):
 			return True
 	return False
 
+def windows_dirname(path):
+	return '\\'.join(path.split('\\')[:-1])
+
 class FileStatus:
 	def __init__(self, name, exists_before, exists_after, modified):
 		self.name = name
@@ -64,6 +68,8 @@ class FileTrackingState:
 	#   new: path was created during the analysis (usually by the sample)
 	#   phantom: path must have been created before the analysis started (eg.
 	#            by recentfiles.py or by Windows)
+	#   inconsistent: impossible operations, indicative of incorrect ProcMon
+	#                 tracking information, were observed for this path
 	# status:
 	#   ignored: file wasn't touched during analysis
 	#   modified: file content was changed
@@ -159,10 +165,12 @@ def load_task_info(file):
 	# time on the VM when it starts, and somehow the filesystem still uses
 	# this timeline even after setting the Windows clock.
 	real_start_time = host_to_utc(isoparse(temp['started_on']['$dt']))
+	stop_time = host_to_utc(isoparse(temp['completed_on']['$dt']))
 	metadata = { key: temp[key] for key in ['route', 'timeout', 'duration', 'id'] }
 	metadata['virtual_start_time'] = virtual_start_time.timestamp()
 	metadata['real_start_time'] = real_start_time.timestamp()
-	return metadata, virtual_start_time, real_start_time
+	metadata['real_stop_time'] = stop_time.timestamp()
+	return metadata, virtual_start_time, real_start_time, stop_time
 
 def load_report(file):
 	with io.open(file, 'rb') as fd:
@@ -244,6 +252,13 @@ class Tracker:
 			del self.original_name[old_normpath]
 		else:
 			self.original_name[new_normpath] = old_path
+		# probably harmless, but good to know when it happens
+		if windows_dirname(new_normpath) != windows_dirname(old_normpath):
+			warn = ('move_across_dir',
+					'moved across directories: %s -> %s at %s'
+					% (old_normpath, new_normpath, time))
+			self.warn[new_normpath].append(warn)
+			self.warn[old_normpath].append(warn)
 
 	def update_times(self, time, path):
 		ts = self.tracking[path.lower()]
@@ -333,11 +348,49 @@ class Stats:
 			return None
 		return { 'mean': self.mean(), 'stdev': self.stdev(), 'n': self.n() }
 
-def format_status(tracking, duration=None):
-	if duration is None:
-		duration = tracking.duration()
-	return { 'file_group': tracking.group, 'status': tracking.status,
-			'time': tracking.time(), 'duration': duration }
+def merge_status(fs, ops, warn):
+	if ops is None:
+		warn.append(('missing_fileops', 'missing in fileops, %s/%s on filesystem'
+				% (fs.group, fs.status)))
+		return fs, None
+	if fs is None:
+		warn.append(('missing_filesystem',
+				'missing in filesystem, fileops indicate %s/%s'
+				% (ops.group, ops.status)))
+		return ops, None
+
+	if fs.status != ops.status:
+		warn.append(('inconsistent_state',
+				'%s/%s on filesystem, but fileops indicate %s/%s' %
+				(fs.group, fs.status, ops.group, ops.status)))
+	# times always differ a bit. statistically monitor that inconsistency.
+	if fs.time() is not None and ops.time() is not None:
+		delta = fs.time() - ops.time()
+		delta_stats.update(delta)
+		if abs(delta) > MAX_TIME_DIFF:
+			warn.append(('timestamp_differs',
+					'timestamp discrepancy too big: %f / %f'
+					% (fs.time(), ops.time())))
+	if fs.duration() is not None and ops.duration() is not None:
+		delta = fs.duration() - ops.duration()
+		duration_stats.update(delta)
+		if abs(delta) > MAX_DURATION_DIFF:
+			warn.append(('duration_differs',
+					'duration discrepancy too big: %f / %f'
+					% (fs.duration(), ops.duration())))
+
+	# fileops is more reliable for duration of operations because it knows
+	# the actual operations that took place. it's also the only way to get
+	# a duration for samples that overwrite existing files.
+	duration = ops.duration() if ops.duration() is not None else fs.duration()
+	# use filesystem status with fileops timestamp. only fileops has
+	# timestamps for deletions, so we need to use them for everything
+	# else, too, for maximum consistency.
+	# assuming that a sample either evades all monitoring or none of it,
+	# either all files hit the "missing in fileops" case, or all have good
+	# fileops values. thus there should never be a mix of files with
+	# fileops and filesystem manifest times.
+	return fs, duration
 
 if len(sys.argv) < 4:
 	sys.stderr.write('usage: python meta.py /path/to/analyses path/to/output task-id...\n')
@@ -379,7 +432,9 @@ for task in tasks:
 		sys.stderr.write('processing module. information will be missing.\n')
 		fileops = []
 
-	task_meta, virtual_start_time, real_start_time = load_task_info(task_info)
+	task_meta, virtual_start_time, real_start_time, real_stop_time = \
+			load_task_info(task_info)
+	virtual_stop_time = virtual_start_time - real_start_time + real_stop_time
 	assert task_meta['id'] == int(task)
 	file_meta, status_manifest, warn_manifest = parse_manifest(base, manifest,
 			task, real_start_time, virtual_start_time)
@@ -394,63 +449,36 @@ for task in tasks:
 
 	delta_stats = Stats()
 	duration_stats = Stats()
+	last_operation = virtual_start_time
 	status = dict()
 	for path in paths:
 		fs = status_manifest.get(path)
 		ops = status_fileops.get(path)
 
-		if ops is None:
-			warnings[path].append(('missing_fileops',
-					'missing in fileops, %s/%s on filesystem'
-					% (fs.group, fs.status)))
-			status[path] = format_status(fs)
-			continue
-		if ops.group == 'phantom':
+		if ops is not None and ops.group == 'phantom':
 			if path in orig_filename:
 				org = 'origin = %s' % orig_filename[path]
 			else:
 				org = 'unknown origin'
 			warnings[path].append(('phantom_file', 'phantom file, %s' % org))
-		if fs is None:
-			warnings[path].append(('missing_filesystem',
-					'missing in filesystem, fileops indicate %s/%s'
-					% (ops.group, ops.status)))
-			status[path] = format_status(ops)
-			continue
 
-		if fs.status != ops.status:
-			warnings[path].append(('inconsistent_state',
-					'%s/%s on filesystem, but fileops indicate %s/%s' %
-					(fs.group, fs.status, ops.group, ops.status)))
+		tracking, duration = merge_status(fs, ops, warnings[path])
+		if duration is None:
+			duration = tracking.duration()
+		status[path] = { 'file_group': tracking.group, 'duration': duration,
+				'status': tracking.status, 'time': tracking.time() }
 
-		# fileops is more reliable for duration of operations because it knows
-		# the actual operations that took place. it's also the only way to get
-		# a duration for samples that overwrite existing files.
-		duration = ops.duration() if ops.duration() is not None else fs.duration()
-		# use filesystem status with fileops timestamp. only fileops has
-		# timestamps for deletions, so we need to use them for everything
-		# else, too, for maximum consistency.
-		# assuming that a sample either evades all monitoring or none of it,
-		# either all files hit the "missing in fileops" case, or all have good
-		# fileops values. thus there should never be a mix of files with
-		# fileops and filesystem manifest times.
-		status[path] = format_status(fs, duration)
-
-		# times always differ a bit. statistically monitor that inconsistency.
-		if fs.time() is not None and ops.time() is not None:
-			delta = fs.time() - ops.time()
-			delta_stats.update(delta)
-			if abs(delta) > MAX_TIME_DIFF:
-				warnings[path].append(('timestamp_differs',
-						'timestamp discrepancy too big: %f / %f'
-						% (fs.time(), ops.time())))
-		if fs.duration() is not None and ops.duration() is not None:
-			delta = fs.duration() - ops.duration()
-			duration_stats.update(delta)
-			if abs(delta) > MAX_DURATION_DIFF:
-				warnings[path].append(('duration_differs',
-						'duration discrepancy too big: %f / %f'
-						% (fs.duration(), ops.duration())))
+		if tracking.end is not None:
+			if tracking.end > last_operation:
+				last_operation = tracking.end
+			if tracking.end > virtual_stop_time:
+				warnings[path].append(('impossible_timestamp',
+						'operation at %f after nominal timeout at %f'
+						% (tracking.time(), virtual_stop_time.timestamp())))
+			elif virtual_stop_time - tracking.end < TIMEOUT_MARGIN:
+				warnings[path].append(('timeout_margin',
+						'operation at %f too close before timeout %f'
+						% (tracking.time(), virtual_stop_time.timestamp())))
 
 	for path in paths:
 		if path not in file_meta:
@@ -482,10 +510,13 @@ for task in tasks:
 	task_meta['timestamp_diff'] = delta_stats.aggregate_statistics()
 	task_meta['duration_diff'] = duration_stats.aggregate_statistics()
 	task_meta['sample'] = targetinfo['md5']
+	task_meta['last_operation'] = last_operation.timestamp()
 	with io.open(os.path.join(taskdir, 'taskinfo.json'), 'w') as fd:
 		json.dump(task_meta, fd)
-	print('#%s: %s, %d warnings' % (task, targetinfo['md5'], nwarn))
+	print('#%s: %s, duration %ds, %d warnings'
+			% (task, targetinfo['md5'], task_meta['duration'], nwarn))
 	print('  time difference: %f ±%f (n=%d)' % (delta_stats.mean(),
 			delta_stats.stdev(), delta_stats.n()))
 	print('  duration difference: %f ±%f (n=%d)' % (duration_stats.mean(),
 			duration_stats.stdev(), duration_stats.n()))
+	print('  margin to analysis timeout: %s' % (virtual_stop_time - last_operation))
